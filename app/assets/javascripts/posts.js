cb44695cd1bbd14af508cc9f81efa20209e872c3
Brooks Swinnerton <bswinnerton@gmail.com>
2013-09-20 12:01:52 -0400

    Responsive masonry WIP

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,11 +1,30 @@
+function checkWidth() {
+  var windowSize = $(window).width();
+  var $postBox =  $('.post-box img')
+
+  if (windowSize < 768) {
+  }
+  else if (windowSize >= 768 && windowSize < 992) {
+    $postBox.css('width', '210')
+  }
+  else if (windowSize >= 992 && windowSize < 1200) {
+    $postBox.css('width', '284')
+  }
+  else if (windowSize >= 1200) {
+    $postBox.css('width', '350')
+  }
+}
+
 $(document).ready(function(){
   var $container = $('.masonry');
 
-  $container.imagesLoaded( function() {
-    $container.masonry({
-      itemSelector        : '.post-box',
-      columnWidth         : '.post-box',
-      transitionDuration  : 0
-    });
+  checkWidth();
+
+  $container.masonry({
+    itemSelector        : '.post-box',
+    columnWidth         : '.post-box',
+    transitionDuration  : 0
   });
+
+  $(window).resize(checkWidth);
 });
